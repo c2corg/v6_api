694847dc3e923025914d44bf8e10a7e2a563fb2f
Guillaume Beraudo <guillaume.beraudo@camptocamp.com>
2015-09-29 17:34:51 +0200

    Port Waypoint test to BaseTestRest

--- c2corg_api/tests/views/test_waypoint.py
+++ c2corg_api/tests/views/test_waypoint.py
@@ -1,57 +1,26 @@
-import json
-
 from c2corg_api.models.waypoint import Waypoint, WaypointLocale
 
-from .. import BaseTestCase
+from c2corg_api.tests.views import BaseTestRest
 
 
-class TestWaypointRest(BaseTestCase):
+class TestWaypointRest(BaseTestRest):
 
     def setUp(self):  # noqa
-        BaseTestCase.setUp(self)
+        self.set_prefix_and_model("/waypoints", Waypoint)
+        BaseTestRest.setUp(self)
         self._add_test_data()
 
     def test_get_collection(self):
-        response = self.app.get('/waypoints')
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.content_type, 'application/json')
-
-        body = json.loads(response.body)
-        self.assertTrue(isinstance(body, list))
-        nb_waypoints = self.session.query(Waypoint).count()
-        self.assertEqual(len(body), nb_waypoints)
+        self.get_collection()
 
     def test_get(self):
-        response = self.app.get('/waypoints/' + str(self.waypoint.document_id))
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.content_type, 'application/json')
-
-        body = json.loads(response.body)
-        self.assertFalse('id' in body)
-        self.assertEqual(body.get('document_id'), self.waypoint.document_id)
-        self.assertEqual(
-            body.get('waypoint_type'), self.waypoint.waypoint_type)
-
-        locales = body.get('locales')
-        self.assertEqual(len(locales), 1)
-        locale_en = locales[0]
-        self.assertFalse('id' in locale_en)
-        self.assertEqual(locale_en.get('culture'), self.locale_en.culture)
-        self.assertEqual(locale_en.get('title'), self.locale_en.title)
+        self.get(self.waypoint)
 
     def test_post_error(self):
-        body = {}
-        response = self.app.post(
-            '/waypoints', params=json.dumps(body), expect_errors=True)
-        self.assertEqual(response.status_code, 400)
-
-        body = json.loads(response.body)
-        self.assertEqual(body.get('status'), 'error')
+        body = self.post_error({})
         errors = body.get('errors')
         self.assertEqual(len(errors), 1)
-        self.assertEqual(
-            errors[0].get('description'), 'waypoint_type is missing')
-        self.assertEqual(errors[0].get('name'), 'waypoint_type')
+        self.assertMissing(errors[0], 'waypoint_type')
 
     def test_post_missing_title(self):
         body = {
@@ -61,12 +30,10 @@ class TestWaypointRest(BaseTestCase):
                 {'culture': 'en'}
             ]
         }
-        response = self.app.post(
-            '/waypoints', params=json.dumps(body), expect_errors=True,
-            content_type='application/json')
-        self.assertEqual(response.status_code, 400)
+        response = self.app.post_json(
+            '/waypoints', body, expect_errors=True, status=400)
 
-        body = json.loads(response.body)
+        body = response.json
         self.assertEqual(body.get('status'), 'error')
         errors = body.get('errors')
         self.assertEqual(len(errors), 1)
@@ -82,37 +49,14 @@ class TestWaypointRest(BaseTestCase):
                  'pedestrian_access': 'y'}
             ]
         }
-        response = self.app.post(
-            '/waypoints', params=json.dumps(body),
-            content_type='application/json')
-        self.assertEqual(response.status_code, 200)
-
-        body = json.loads(response.body)
-        document_id = body.get('document_id')
-        self.assertIsNotNone(document_id)
-
-        # check that the version was created correctly
-        waypoint = self.session.query(Waypoint).get(document_id)
-        versions = waypoint.versions
-        self.assertEqual(len(versions), 1)
-        version = versions[0]
-
-        self.assertEqual(version.culture, 'en')
-        self.assertEqual(version.version, 1)
-        self.assertEqual(version.nature, 'ft')
-
-        meta_data = version.history_metadata
-        self.assertEqual(meta_data.is_minor, False)
-        self.assertEqual(meta_data.comment, 'creation')
-        self.assertIsNotNone(meta_data.written_at)
+        body, doc = self.post_success(body)
+        version = doc.versions[0]
 
         archive_waypoint = version.document_archive
-        self.assertEqual(archive_waypoint.document_id, document_id)
         self.assertEqual(archive_waypoint.waypoint_type, 'summit')
         self.assertEqual(archive_waypoint.elevation, 3779)
 
         archive_locale = version.document_locales_archive
-        self.assertEqual(archive_locale.document_id, document_id)
         self.assertEqual(archive_locale.culture, 'en')
         self.assertEqual(archive_locale.title, 'Mont Pourri')
         self.assertEqual(archive_locale.pedestrian_access, 'y')
