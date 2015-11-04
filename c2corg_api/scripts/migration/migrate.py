a8cc19d55b35f3de802d63d85c55b679fcf4606c
tsauerwein <tobias.sauerwein@camptocamp.com>
2015-11-04 15:43:46 +0100

    Update sequences after import

--- c2corg_api/scripts/migration/migrate.py
+++ c2corg_api/scripts/migration/migrate.py
@@ -1,19 +1,31 @@
-import os
 import sys
 import logging
-from sqlalchemy.orm import sessionmaker
-from zope.sqlalchemy import ZopeTransactionExtension
 from sqlalchemy import engine_from_config
+
+import os
+from sqlalchemy.orm import sessionmaker
+
 from pyramid.paster import get_appsettings
 
+from zope.sqlalchemy import ZopeTransactionExtension
 from c2corg_api.models import Base
 from c2corg_api.scripts.initializedb import setup_db
-from c2corg_api.scripts.migration.documents.summit import MigrateSummits
 from c2corg_api.scripts.migration.documents.versions import MigrateVersions
+from c2corg_api.scripts.migration.documents.waypoints.huts import MigrateHuts
+from c2corg_api.scripts.migration.documents.waypoints.parking import \
+    MigrateParkings
+from c2corg_api.scripts.migration.documents.waypoints.products import \
+    MigrateProducts
+from c2corg_api.scripts.migration.documents.waypoints.sites import MigrateSites
+from c2corg_api.scripts.migration.documents.waypoints.summit import \
+    MigrateSummits
 
 
 # no-op function referenced from `migration.ini` (required for
 # `get_appsettings` to work)
+from c2corg_api.scripts.migration.sequences import UpdateSequences
+
+
 def no_op(global_config, **settings): pass
 
 
@@ -38,7 +50,12 @@ def main(argv=sys.argv):
 
     batch_size = 1000
     MigrateSummits(connection_source, session, batch_size).migrate()
+    MigrateParkings(connection_source, session, batch_size).migrate()
+    MigrateSites(connection_source, session, batch_size).migrate()
+    MigrateProducts(connection_source, session, batch_size).migrate()
+    MigrateHuts(connection_source, session, batch_size).migrate()
     MigrateVersions(connection_source, session, batch_size).migrate()
+    UpdateSequences(connection_source, session, batch_size).migrate()
 
 if __name__ == "__main__":
     main()
