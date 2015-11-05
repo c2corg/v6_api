7faa65ccb51c041434c05a17b7578829210343b0
Guillaume Beraudo <guillaume.beraudo@camptocamp.com>
2015-11-05 13:51:48 +0100

    Allow creating c2corg_tests database
    It is now possible to pass USER="" to the script.

--- scripts/create_user_db_test.sh
+++ scripts/create_user_db_test.sh
@@ -3,9 +3,12 @@
 PSQL=psql
 [ "$USER" != "travis" ] && PSQL="sudo -u postgres psql"
 
+DBNAME="c2corg_${USER}_tests"
+[ -z "$USER" ] && DBNAME="c2corg_tests"
+
 $PSQL <<EOF
-create database c2corg_${USER}_tests owner "www-data";
-\c c2corg_${USER}_tests
+create database ${DBNAME} owner "www-data";
+\c ${DBNAME}
 create extension postgis;
 create schema guidebook authorization "www-data";
 \q
