118a00296aeffca15ee0ab36bdff837c765e7294
tsauerwein <tobias.sauerwein@camptocamp.com>
2015-11-06 10:44:37 +0100

    Migrate routes

--- c2corg_api/scripts/migration/documents/versions.py
+++ c2corg_api/scripts/migration/documents/versions.py
@@ -6,10 +6,10 @@ from c2corg_api.models.document_history import HistoryMetaData, DocumentVersion
 from c2corg_api.scripts.migration.batch import SimpleBatch
 from c2corg_api.scripts.migration.migrate_base import MigrateBase
 
-# TODO only importing the versions of waypoints
+# TODO only importing the versions of waypoints and routes
 tables = [
     'app_huts_archives', 'app_parkings_archives', 'app_products_archives',
-    'app_sites_archives', 'app_summits_archives'
+    'app_sites_archives', 'app_summits_archives', 'app_routes_archives'
 ]
 tables_union = ' union '.join(['select id from ' + t for t in tables])
 
