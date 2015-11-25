0704334ff1aec587f0c9bf4c3ff574ade2f22319
Guillaume Beraudo <guillaume.beraudo@camptocamp.com>
2015-11-25 14:57:46 +0100

    Add index to token expire column

--- c2corg_api/models/token.py
+++ c2corg_api/models/token.py
@@ -17,6 +17,6 @@ class Token(Base):
     __table_args__ = {"schema": users_schema}
 
     value = Column(String(), nullable=False, primary_key=True)
-    expire = Column(DateTime, nullable=False, default=False)
+    expire = Column(DateTime, nullable=False, default=False, index=True)
     userid = Column(Integer, ForeignKey(users_schema + '.user.id'),
                     nullable=False)
