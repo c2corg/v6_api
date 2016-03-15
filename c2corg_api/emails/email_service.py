4d611361aac513439438412174750e34f093d085
Guillaume Beraudo <guillaume.beraudo@camptocamp.com>
2016-03-15 21:58:44 +0100

    Use pyramid.mailer and add tests

--- c2corg_api/emails/email_service.py
+++ c2corg_api/emails/email_service.py
@@ -1,67 +1,58 @@
-import smtplib
-from email.mime.text import MIMEText
+from pyramid_mailer import get_mailer
+from pyramid_mailer.message import Message
 
 import logging
 
 log = logging.getLogger(__name__)
 
 
+# See https://docs.python.org/3/library/smtplib.html#smtplib.SMTP.sendmail
+# https://github.com/Pylons/pyramid_mailer
+# http://docs.pylonsproject.org/projects/pyramid/en/latest/narr/i18n.html
 class EmailService:
+    instance = None
 
-    def _(self, lang, key):
-        extended_key = lang + '_' + key
-        if extended_key in settings:
-            return settings[extended_key]
-        log.warn('Untranslated string ' + extended_key)
-        return settings['fr_' + key]
-
-
-    def __init__(self, settings):
+    def __init__(self, mailer, settings):
         self.mail_from = settings['mail.from']
-        self.mail_host = settings['mail.host']
-        self.mail_port = int(settings.get('mail.port', 0))
-        self.mail_debug = int(settings.get('mail.debug', 0))
+        self.mailer = mailer
         self.settings = settings
 
-
     def _send_email(self, to_address, subject=None, body=None):
-        """Send an email using smtplib.SMTP.sendmail. This method may throw.
-        Use https://docs.python.org/3/library/smtplib.html#smtplib.SMTP.sendmail
-        an alternative would be to use https://github.com/Pylons/pyramid_mailer.
-        """
-        msg = MIMEText(body, "plain", "utf-8")
-        msg['Subject'] = subject
-        msg['From'] = self.mail_from
-        msg['To'] = to_address
-        s = smtplib.SMTP(self.mail_host, self.mail_port)
-        s.set_debuglevel(self.mail_debug)
-        s.sendmail(self.mail_from, to_address, msg.as_string())
-        s.quit()
-
+        """Send an email. This method may throw."""
+        msg = Message(
+                subject=subject,
+                sender=self.mail_from,
+                recipients=[to_address],
+                body=body)
+        self.mailer.send_immediately(msg)
 
     def send_registration_confirmation(self, lang, user, link):
         # TODO: handle i18n
-        # http://docs.pylonsproject.org/projects/pyramid/en/latest/narr/i18n.html
         self._send_email(
                 user.email,
-                subject= 'Inscription sur Camptocamp.org',
+                subject='Inscription sur Camptocamp.org',
                 body='Pour activer votre compte cliquez sur %s' % link)
 
 
-email_service_instance = None
 def get_email_service(request):
-    if not email_service_instance:
-        email_service_instance = EmailService(request.registry.settings)
-    return email_service_instance
+    if not EmailService.instance:
+        mailer = get_mailer(request)
+        settings = request.registry.settings
+        EmailService.instance = EmailService(mailer, settings)
+    return EmailService.instance
 
 
 if __name__ == '__main__':
-    import os, sys
+    import os
+    import sys
     from pyramid.paster import get_appsettings
-    from c2corg_api.models.user import User
+    from pyramid_mailer.mailer import Mailer
     curdir = os.path.dirname(os.path.abspath(__file__))
     configfile = os.path.realpath(os.path.join(curdir, '../../common.ini'))
-    EmailService(get_appsettings(configfile))._send_email(
+    settings = get_appsettings(configfile)
+    mailer = Mailer.from_settings(settings)
+
+    EmailService(mailer, settings)._send_email(
             sys.argv[1],
-            subject='Test send email',
-            body='body http://localhost')
+            subject='Test send email élève forêt ça alors',
+            body='body 日本国 http://localhost')
