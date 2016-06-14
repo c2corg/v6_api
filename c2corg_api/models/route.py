72e8aa083a3ac41933ca75e715a793fbfa3d5c8e
tsauerwein <tobias.sauerwein@camptocamp.com>
2016-06-14 13:31:10 +0200

    Fix problem with saving `durations`

--- c2corg_api/models/route.py
+++ c2corg_api/models/route.py
@@ -150,9 +150,9 @@ class _RouteMixin(object):
 
 attributes = [
     'main_waypoint_id', 'activities', 'elevation_min', 'elevation_max',
-    'height_diff_up', 'height_diff_down', 'route_length',
+    'height_diff_up', 'height_diff_down', 'route_length', 'durations',
     'difficulties_height', 'height_diff_access', 'height_diff_difficulties',
-    'route_types', 'orientations', 'duration', 'glacier_gear', 'configuration',
+    'route_types', 'orientations', 'glacier_gear', 'configuration',
     'lift_access', 'ski_rating', 'ski_exposition', 'labande_ski_rating',
     'labande_global_rating', 'global_rating', 'engagement_rating',
     'risk_rating', 'equipment_rating', 'ice_rating', 'mixed_rating',
