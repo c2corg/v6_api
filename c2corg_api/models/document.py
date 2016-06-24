d39439e02accfcef1eb7340502b531fd795c99b1
tsauerwein <tobias.sauerwein@camptocamp.com>
2016-06-24 12:00:50 +0200

    Do not create spatial index for archive geometry table

--- c2corg_api/models/document.py
+++ c2corg_api/models/document.py
@@ -285,7 +285,9 @@ class _DocumentGeometryMixin(object):
     @declared_attr
     def geom(self):
         return Column(
-            Geometry(geometry_type='POINT', srid=3857, management=True),
+            Geometry(
+                geometry_type='POINT', srid=3857, management=True,
+                spatial_index=self.__name__ != 'ArchiveDocumentGeometry'),
             info={
                 'colanderalchemy': {
                     'typ': colander_ext.Geometry('POINT', srid=3857)
@@ -297,7 +299,9 @@ class _DocumentGeometryMixin(object):
     @declared_attr
     def geom_detail(self):
         return Column(
-            Geometry(geometry_type='GEOMETRY', srid=3857, management=True),
+            Geometry(
+                geometry_type='GEOMETRY', srid=3857, management=True,
+                spatial_index=self.__name__ != 'ArchiveDocumentGeometry'),
             info={
                 'colanderalchemy': {
                     'typ': colander_ext.Geometry('GEOMETRY', srid=3857)
