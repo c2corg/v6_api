a22ba0566b1ad6e02d2ca4727a3cc74ced04e6d8
arnaud.morvan@camptocamp.com <arnaud.morvan@camptocamp.com>
2016-06-24 16:11:42 +0200

    Fix existing tests and add new images related ones

--- c2corg_api/tests/views/test_image.py
+++ c2corg_api/tests/views/test_image.py
@@ -1,4 +1,5 @@
 import json
+from unittest.mock import patch, Mock
 
 from c2corg_api.models.association import Association
 from c2corg_api.models.waypoint import Waypoint, WaypointLocale
@@ -14,12 +15,121 @@ from c2corg_api.views.document import DocumentRest
 from c2corg_api.tests.views import BaseDocumentTestRest
 
 
-class TestImageRest(BaseDocumentTestRest):
+class BaseTestImage(BaseDocumentTestRest):
+
+    def _add_test_data(self):
+        self.image = Image(
+            filename='image.jpg',
+            activities=['paragliding'], height=1500,
+            image_type='collaborative')
+
+        self.locale_en = DocumentLocale(
+            lang='en', title='Mont Blanc from the air', description='...')
+
+        self.locale_fr = DocumentLocale(
+            lang='fr', title='Mont Blanc du ciel', description='...')
+
+        self.image.locales.append(self.locale_en)
+        self.image.locales.append(self.locale_fr)
+
+        self.image.geometry = DocumentGeometry(
+            geom='SRID=3857;POINT(635956 5723604)')
+
+        self.session.add(self.image)
+        self.session.flush()
+
+        user_id = self.global_userids['contributor']
+        DocumentRest.create_new_version(self.image, user_id)
+
+        self.image2 = Image(
+            filename='image2.jpg',
+            activities=['paragliding'], height=1500)
+        self.session.add(self.image2)
+        self.image3 = Image(
+            filename='image3.jpg',
+            activities=['paragliding'], height=1500)
+        self.session.add(self.image3)
+        self.image4 = Image(
+            filename='image4.jpg',
+            activities=['paragliding'], height=1500,
+            image_type='personal')
+        self.image4.locales.append(DocumentLocale(
+            lang='en', title='Mont Blanc from the air', description='...'))
+        self.image4.locales.append(DocumentLocale(
+            lang='fr', title='Mont Blanc du ciel', description='...'))
+        self.session.add(self.image4)
+        self.session.flush()
+        DocumentRest.create_new_version(self.image4, user_id)
+
+        self.waypoint = Waypoint(
+            waypoint_type='summit', elevation=4,
+            geometry=DocumentGeometry(
+                geom='SRID=3857;POINT(635956 5723604)'))
+        self.waypoint.locales.append(WaypointLocale(
+            lang='en', title='Mont Granier (en)', description='...',
+            access='yep'))
+        self.session.add(self.waypoint)
+        self.session.flush()
+
+    def _post_success_document(self):
+        return {
+            'filename': 'post_image.jpg',
+            'activities': ['paragliding'],
+            'image_type': 'collaborative',
+            'height': 1500,
+            'geometry': {
+                'id': 5678, 'version': 6789,
+                'geom': '{"type": "Point", "coordinates": [635956, 5723604]}'
+            },
+            'locales': [
+                {'lang': 'en', 'title': 'Some nice loop'}
+            ],
+            'associations': {
+                'waypoints': [{'document_id': self.waypoint.document_id}]
+            }
+        }
+
+    def _validate_post_success(self, body, doc):
+        self._assert_geometry(body)
+
+        version = doc.versions[0]
+
+        archive_image = version.document_archive
+        self.assertEqual(archive_image.activities, ['paragliding'])
+        self.assertEqual(archive_image.height, 1500)
+
+        archive_locale = version.document_locales_archive
+        self.assertEqual(archive_locale.lang, 'en')
+        self.assertEqual(archive_locale.title, 'Some nice loop')
+
+        archive_geometry = version.document_geometry_archive
+        self.assertEqual(archive_geometry.version, doc.geometry.version)
+        self.assertIsNotNone(archive_geometry.geom)
+
+        # check that a link to the linked wp is created
+        association_wp = self.session.query(Association).get(
+            (self.waypoint.document_id, doc.document_id))
+        self.assertIsNotNone(association_wp)
+
+    def _assert_geometry(self, body):
+        self.assertIsNotNone(body.get('geometry'))
+        geometry = body.get('geometry')
+        self.assertIsNotNone(geometry.get('version'))
+        self.assertIsNotNone(geometry.get('geom'))
+
+        geom = geometry.get('geom')
+        point = shape(json.loads(geom))
+        self.assertIsInstance(point, Point)
+        self.assertAlmostEqual(point.x, 635956)
+        self.assertAlmostEqual(point.y, 5723604)
+
+
+class TestImageRest(BaseTestImage):
 
     def setUp(self):  # noqa
         self.set_prefix_and_model(
             "/images", IMAGE_TYPE, Image, ArchiveImage, ArchiveDocumentLocale)
-        BaseDocumentTestRest.setUp(self)
+        BaseTestImage.setUp(self)
         self._add_test_data()
 
     def test_get_collection(self):
@@ -86,12 +196,14 @@ class TestImageRest(BaseDocumentTestRest):
     def test_post_error(self):
         body = self.post_error({})
         errors = body.get('errors')
-        self.assertEqual(len(errors), 2)
-        self.assertCorniceRequired(errors[0], 'locales')
-        self.assertCorniceRequired(errors[1], 'image_type')
+        self.assertEqual(len(errors), 3)
+        self.assertCorniceMissing(errors[0], 'filename')
+        self.assertCorniceRequired(errors[1], 'locales')
+        self.assertCorniceRequired(errors[2], 'image_type')
 
     def test_post_missing_title(self):
         body_post = {
+            'filename': 'post_missing_title.jpg',
             'activities': ['paragliding'],
             'image_type': 'collaborative',
             'height': 1500,
@@ -103,8 +215,11 @@ class TestImageRest(BaseDocumentTestRest):
         errors = body.get('errors')
         self.assertEqual(len(errors), 2)
 
-    def test_post_non_whitelisted_attribute(self):
+    @patch('c2corg_api.views.image.requests.post',
+           return_value=Mock(status_code=200))
+    def test_post_non_whitelisted_attribute(self, post_mock):
         body = {
+            'filename': 'post_non_whitelisted.jpg',
             'activities': ['paragliding'],
             'image_type': 'collaborative',
             'height': 1500,
@@ -118,49 +233,42 @@ class TestImageRest(BaseDocumentTestRest):
     def test_post_missing_content_type(self):
         self.post_missing_content_type({})
 
-    def test_post_success(self):
-        body = {
-            'activities': ['paragliding'],
-            'image_type': 'collaborative',
-            'height': 1500,
-            'geometry': {
-                'id': 5678, 'version': 6789,
-                'geom': '{"type": "Point", "coordinates": [635956, 5723604]}'
-            },
-            'locales': [
-                {'lang': 'en', 'title': 'Some nice loop'}
-            ],
-            'associations': {
-                'waypoints': [{'document_id': self.waypoint.document_id}]
-            }
-        }
-        body, doc = self.post_success(body)
-        self._assert_geometry(body)
+    def test_post_missing_filename(self):
+        body_post = self._post_success_document()
+        del body_post['filename']
+        body = self.post_error(body_post)
+        errors = body.get('errors')
+        self.assertCorniceMissing(errors[0], 'filename')
 
-        version = doc.versions[0]
+    def test_post_duplicated_filename(self):
+        body_post = self._post_success_document()
+        body_post['filename'] = 'image.jpg'
+        body = self.post_error(body_post)
+        errors = body.get('errors')
+        self.assertEqual(errors[0].get('description'), 'Unique')
 
-        archive_image = version.document_archive
-        self.assertEqual(archive_image.activities, ['paragliding'])
-        self.assertEqual(archive_image.height, 1500)
+    @patch('c2corg_api.views.image.requests.post',
+           return_value=Mock(status_code=500, reason='test error'))
+    def test_post_image_backend_error(self, post_mock):
+        headers = self.add_authorization_header(username='contributor')
+        r = self.app_post_json(self._prefix,
+                               self._post_success_document(),
+                               headers=headers,
+                               status=500)
+        self.assertIn('test error', r.text)
 
-        archive_locale = version.document_locales_archive
-        self.assertEqual(archive_locale.lang, 'en')
-        self.assertEqual(archive_locale.title, 'Some nice loop')
-
-        archive_geometry = version.document_geometry_archive
-        self.assertEqual(archive_geometry.version, doc.geometry.version)
-        self.assertIsNotNone(archive_geometry.geom)
-
-        # check that a link to the linked wp is created
-        association_wp = self.session.query(Association).get(
-            (self.waypoint.document_id, doc.document_id))
-        self.assertIsNotNone(association_wp)
+    @patch('c2corg_api.views.image.requests.post',
+           return_value=Mock(status_code=200))
+    def test_post_success(self, post_mock):
+        body, doc = self.post_success(self._post_success_document())
+        self._validate_post_success(body, doc)
 
     def test_put_wrong_document_id(self):
         body = {
             'document': {
                 'document_id': '-9999',
                 'version': self.image.version,
+                'filename': 'put_wrong_document_id.jpg',
                 'activities': ['paragliding'],
                 'image_type': 'collaborative',
                 'height': 1500,
@@ -178,6 +286,7 @@ class TestImageRest(BaseDocumentTestRest):
             'document': {
                 'document_id': self.image.document_id,
                 'version': -9999,
+                'filename': 'put_wrong_document_version.jpg',
                 'activities': ['paragliding'],
                 'image_type': 'collaborative',
                 'height': 1500,
@@ -195,6 +304,7 @@ class TestImageRest(BaseDocumentTestRest):
             'document': {
                 'document_id': self.image.document_id,
                 'version': self.image.version,
+                'filename': self.image.filename,
                 'activities': ['paragliding'],
                 'image_type': 'collaborative',
                 'height': 1500,
@@ -212,6 +322,7 @@ class TestImageRest(BaseDocumentTestRest):
             'document': {
                 'document_id': self.image.document_id,
                 'version': self.image.version,
+                'filename': self.image.filename,
                 'activities': ['paragliding'],
                 'image_type': 'collaborative',
                 'height': 1500,
@@ -236,6 +347,7 @@ class TestImageRest(BaseDocumentTestRest):
             'document': {
                 'document_id': self.image4.document_id,
                 'version': self.image4.version,
+                'filename': self.image4.filename,
                 'activities': ['skitouring'],
                 'image_type': 'personal',
                 'height': 2000,
@@ -260,6 +372,7 @@ class TestImageRest(BaseDocumentTestRest):
             'document': {
                 'document_id': self.image4.document_id,
                 'version': self.image4.version,
+                'filename': self.image4.filename,
                 'quality': quality_types[1],
                 'activities': ['skitouring'],
                 'image_type': 'personal',
@@ -282,6 +395,7 @@ class TestImageRest(BaseDocumentTestRest):
             'document': {
                 'document_id': self.image.document_id,
                 'version': self.image.version,
+                'filename': self.image.filename,
                 'quality': quality_types[1],
                 'activities': ['paragliding'],
                 'image_type': 'collaborative',
@@ -335,6 +449,7 @@ class TestImageRest(BaseDocumentTestRest):
             'document': {
                 'document_id': self.image.document_id,
                 'version': self.image.version,
+                'filename': self.image.filename,
                 'quality': quality_types[1],
                 'activities': ['paragliding'],
                 'image_type': 'collaborative',
@@ -356,6 +471,7 @@ class TestImageRest(BaseDocumentTestRest):
             'document': {
                 'document_id': self.image.document_id,
                 'version': self.image.version,
+                'filename': self.image.filename,
                 'quality': quality_types[1],
                 'activities': ['paragliding'],
                 'image_type': 'collaborative',
@@ -380,6 +496,7 @@ class TestImageRest(BaseDocumentTestRest):
             'document': {
                 'document_id': self.image.document_id,
                 'version': self.image.version,
+                'filename': self.image.filename,
                 'quality': quality_types[1],
                 'activities': ['paragliding'],
                 'image_type': 'collaborative',
@@ -403,6 +520,7 @@ class TestImageRest(BaseDocumentTestRest):
             'document': {
                 'document_id': self.image.document_id,
                 'version': self.image.version,
+                'filename': self.image.filename,
                 'quality': quality_types[1],
                 'activities': ['paragliding'],
                 'image_type': 'personal',
@@ -428,6 +546,7 @@ class TestImageRest(BaseDocumentTestRest):
             'document': {
                 'document_id': self.image.document_id,
                 'version': self.image.version,
+                'filename': self.image.filename,
                 'quality': quality_types[1],
                 'activities': ['paragliding'],
                 'image_type': 'personal',
@@ -452,6 +571,7 @@ class TestImageRest(BaseDocumentTestRest):
             'document': {
                 'document_id': self.image4.document_id,
                 'version': self.image4.version,
+                'filename': self.image4.filename,
                 'quality': quality_types[1],
                 'activities': ['paragliding'],
                 'image_type': 'collaborative',
@@ -468,64 +588,36 @@ class TestImageRest(BaseDocumentTestRest):
             self._prefix + '/' + str(self.image4.document_id), body,
             headers=headers, status=200)
 
-    def _assert_geometry(self, body):
-        self.assertIsNotNone(body.get('geometry'))
-        geometry = body.get('geometry')
-        self.assertIsNotNone(geometry.get('version'))
-        self.assertIsNotNone(geometry.get('geom'))
 
-        geom = geometry.get('geom')
-        point = shape(json.loads(geom))
-        self.assertIsInstance(point, Point)
-        self.assertAlmostEqual(point.x, 635956)
-        self.assertAlmostEqual(point.y, 5723604)
+class TestImageListRest(BaseTestImage):
 
-    def _add_test_data(self):
-        self.image = Image(
-            activities=['paragliding'], height=1500,
-            image_type='collaborative')
+    def setUp(self):  # noqa
+        self.set_prefix_and_model(
+            "/images", IMAGE_TYPE, Image, ArchiveImage, ArchiveDocumentLocale)
+        BaseTestImage.setUp(self)
+        self._add_test_data()
 
-        self.locale_en = DocumentLocale(
-            lang='en', title='Mont Blanc from the air', description='...')
+    def post_success(self, request_body, user='contributor'):
+        response = self.app_post_json('/images/list', request_body,
+                                      status=403)
 
-        self.locale_fr = DocumentLocale(
-            lang='fr', title='Mont Blanc du ciel', description='...')
+        headers = self.add_authorization_header(username=user)
+        response = self.app_post_json('/images/list', request_body,
+                                      headers=headers, status=200)
 
-        self.image.locales.append(self.locale_en)
-        self.image.locales.append(self.locale_fr)
+        body = response.json
+        images = body.get("images")
+        self.assertEqual(len(request_body['images']), len(images))
+        for image in images:
+            (body, doc) = self._validate_document(image)
 
-        self.image.geometry = DocumentGeometry(
-            geom='SRID=3857;POINT(635956 5723604)')
+        return (body, doc)
 
-        self.session.add(self.image)
-        self.session.flush()
-
-        user_id = self.global_userids['contributor']
-        DocumentRest.create_new_version(self.image, user_id)
-
-        self.image2 = Image(
-            activities=['paragliding'], height=1500)
-        self.session.add(self.image2)
-        self.image3 = Image(
-            activities=['paragliding'], height=1500)
-        self.session.add(self.image3)
-        self.image4 = Image(
-            activities=['paragliding'], height=1500,
-            image_type='personal')
-        self.image4.locales.append(DocumentLocale(
-            lang='en', title='Mont Blanc from the air', description='...'))
-        self.image4.locales.append(DocumentLocale(
-            lang='fr', title='Mont Blanc du ciel', description='...'))
-        self.session.add(self.image4)
-        self.session.flush()
-        DocumentRest.create_new_version(self.image4, user_id)
-
-        self.waypoint = Waypoint(
-            waypoint_type='summit', elevation=4,
-            geometry=DocumentGeometry(
-                geom='SRID=3857;POINT(635956 5723604)'))
-        self.waypoint.locales.append(WaypointLocale(
-            lang='en', title='Mont Granier (en)', description='...',
-            access='yep'))
-        self.session.add(self.waypoint)
-        self.session.flush()
+    @patch('c2corg_api.views.image.requests.post',
+           return_value=Mock(status_code=200))
+    def test_post_success(self, post_mock):
+        body = {
+            'images': [self._post_success_document()]
+        }
+        body, doc = self.post_success(body)
+        self._validate_post_success(body, doc)
