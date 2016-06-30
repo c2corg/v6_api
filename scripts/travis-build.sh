2f05fcfc525c1903b1d92b4596d27d1da258fb48
Marc Fournier <marc.fournier@camptocamp.org>
2016-06-30 14:31:30 +0200

    improve console output of travis-*.sh scripts

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -3,13 +3,13 @@
 REPO="c2corg/v6_api"
 
 if [ "$TRAVIS_BRANCH" = "master" ]; then
-  echo "Building image with tag latest"
+  echo "Building image '${REPO}:latest'"
   docker build -t "${REPO}:latest" .
 elif [ ! -z "$TRAVIS_TAG" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-  echo "Building image with tag ${TRAVIS_TAG}"
+  echo "Building image '${REPO}:${TRAVIS_TAG}'"
   docker build -t "${REPO}:${TRAVIS_TAG}" .
 elif [ ! -z "$TRAVIS_BRANCH" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-  echo "Building image with tag ${TRAVIS_BRANCH}"
+  echo "Building image '${REPO}:${TRAVIS_BRANCH}'"
   docker build -t "${REPO}:${TRAVIS_BRANCH}" .
 else
   echo "Don't know how to build image"
--- scripts/travis-publish.sh
+++ scripts/travis-publish.sh
@@ -5,14 +5,14 @@ REPO="c2corg/v6_api"
 docker login -e "$DOCKER_EMAIL" -u "$DOCKER_USER" -p "$DOCKER_PASS"
 
 if [ "$TRAVIS_BRANCH" = "master" ]; then
-  echo "Deploying image to docker hub for master (latest)"
+  echo "Pushing image '${REPO}:latest' to docker hub"
   docker push "${REPO}:latest"
 elif [ ! -z "$TRAVIS_TAG" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-  echo "Deploying image to docker hub for tag ${TRAVIS_TAG}"
+  echo "Pushing image '${REPO}:${TRAVIS_TAG}' to docker hub"
   docker push "${REPO}:${TRAVIS_TAG}"
 elif [ ! -z "$TRAVIS_BRANCH" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-  echo "Deploying image to docker hub for branch ${TRAVIS_BRANCH}"
+  echo "Pushing image '${REPO}:${TRAVIS_BRANCH}' to docker hub"
   docker push "${REPO}:${TRAVIS_BRANCH}"
 else
-  echo "Not deploying image"
+  echo "Not pushing any image"
 fi
