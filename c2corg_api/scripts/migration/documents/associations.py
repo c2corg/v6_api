159d031d2e570f78934b0aac534031ee5038e050
tsauerwein <tobias.sauerwein@camptocamp.com>
2016-11-21 09:56:12 +0100

    Create dummy routes for climbing sites

--- c2corg_api/scripts/migration/documents/associations.py
+++ c2corg_api/scripts/migration/documents/associations.py
@@ -72,7 +72,6 @@ class MigrateAssociations(MigrateBase):
             association_log_query_count, association_log_query, AssociationLog,
             self.get_log)
         self._set_route_main_waypoint()
-        self._set_route_locale_title_prefix()
 
     def get_association(self, row):
         parent_type, child_type = self._convert_type(row)
@@ -143,21 +142,6 @@ class MigrateAssociations(MigrateBase):
             zope.sqlalchemy.mark_changed(self.session_target)
         print('Done')
 
-    def _set_route_locale_title_prefix(self):
-        """Set the `title_prefix` field for all route locales that have a
-        main waypoint. First set the field for route locales, where there is
-        a locale of the main waypoint in the same lang. Then with a 2nd
-        query set the remaining rows by selecting the "best" locale of the main
-        waypoint.
-        """
-        with transaction.manager:
-            print('Set title prefix for route locales (same lang)')
-            self.session_target.execute(SQL_SET_TITLE_PREFIX_SAME_CULTURE)
-            print('Set title prefix for route locales (other lang)')
-            self.session_target.execute(SQL_SET_TITLE_PREFIX_OTHER_CULTURE)
-            zope.sqlalchemy.mark_changed(self.session_target)
-        print('Done')
-
     def _convert_type(self, row):
         old_type = row.type
         old_parent_type = old_type[0]
@@ -215,43 +199,6 @@ update guidebook.routes r
   set main_waypoint_id = v.w_id
 from v
 where v.r_id = r.document_id;"""
-
-SQL_SET_TITLE_PREFIX_SAME_CULTURE = """
-with v as (select rl.id, l2.title
-  from guidebook.routes_locales rl join guidebook.documents_locales l1
-    on rl.id = l1.id
-  join guidebook.routes r on l1.document_id = r.document_id
-  join guidebook.documents_locales l2
-    on r.main_waypoint_id = l2.document_id and l2.lang = l1.lang)
-update guidebook.routes_locales l
-  set title_prefix = v.title
-from v
-where v.id = l.id;
-"""
-
-SQL_SET_TITLE_PREFIX_OTHER_CULTURE = """
-with v as (select t.id, t.title
-  from (select rl.id, l2.title, dense_rank() over(
-    partition by rl.id
-    order by
-      l2.lang != 'fr',
-      l2.lang != 'en',
-      l2.lang != 'it',
-      l2.lang != 'de',
-      l2.lang != 'es',
-      l2.lang != 'ca'
-    ) as rank
-    from guidebook.routes_locales rl join guidebook.documents_locales l1
-      on rl.id = l1.id and rl.title_prefix is null
-    join guidebook.routes r on l1.document_id = r.document_id
-    join guidebook.documents_locales l2
-      on r.main_waypoint_id = l2.document_id) t
-  where t.rank = 1)
-update guidebook.routes_locales l
-  set title_prefix = v.title
-from v
-where v.id = l.id;
-"""
 
 
 SQL_DELETE_DUPLICATE_ASSOCIATONS_LOG = """
