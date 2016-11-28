50e1f88b12bc468dd9aa126bb23c1a4db21b8f07
David Kocich <david.kocich@camptocamp.com>
2016-11-28 11:57:24 +0100

    Search and sort reports by date to show them in cards correctly

--- c2corg_api/views/document_listings.py
+++ c2corg_api/views/document_listings.py
@@ -6,6 +6,7 @@ from c2corg_api.models.cache_version import get_document_id, \
 from c2corg_api.models.document import (
     set_available_langs)
 from c2corg_api.models.outing import Outing
+from c2corg_api.models.xreport import Xreport
 from c2corg_api.models.user import User
 from c2corg_api.models.user_profile import UserProfile
 from c2corg_api.views import to_json_dict, set_best_locale
@@ -35,6 +36,10 @@ def get_documents(documents_config, meta_params, search_documents):
     if documents_config.clazz == Outing:
         base_query = base_query. \
             order_by(documents_config.clazz.date_end.desc()). \
+            order_by(documents_config.clazz.document_id.desc())
+    elif documents_config.clazz == Xreport:
+        base_query = base_query. \
+            order_by(documents_config.clazz.date.desc()). \
             order_by(documents_config.clazz.document_id.desc())
     else:
         base_query = base_query.order_by(
