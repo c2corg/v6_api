1c2fbb7969e67d9e7e537851841b7685ccb33bd8
David Kocich <david.kocich@camptocamp.com>
2016-12-07 14:18:45 +0100

    Filter xreports by userid sent in a request

--- c2corg_api/views/xreport.py
+++ c2corg_api/views/xreport.py
@@ -1,6 +1,9 @@
 import functools
 
+from c2corg_api.models import DBSession
+from c2corg_api.models.association import Association
 from c2corg_api.models.document_history import has_been_created_by
+from c2corg_api.models.user import User
 from c2corg_api.models.xreport import (
   Xreport,
   schema_xreport,
@@ -22,7 +25,7 @@ from c2corg_api.views import cors_policy, restricted_json_view, \
     set_private_cache_header
 from c2corg_api.views.validation import validate_id, validate_pagination, \
     validate_lang_param, validate_preferred_lang_param, \
-    validate_associations, validate_lang, validate_version_id
+    validate_associations, validate_lang, validate_version_id, validate_user_id
 
 from pyramid.httpexceptions import HTTPForbidden
 
@@ -32,6 +35,71 @@ validate_associations_create = functools.partial(
     validate_associations, XREPORT_TYPE, True)
 validate_associations_update = functools.partial(
     validate_associations, XREPORT_TYPE, False)
+
+def get_xreports_for_user(request, **kwargs):
+    """ Get reports written by the user if he exists.
+    """
+    user_id = request.validated['u']
+
+    user_exists_query = DBSession.query(User). \
+        filter(User.id == user_id). \
+        exists()
+    user_exists = DBSession.query(user_exists_query).scalar()
+
+    xreports_for_user_info = DBSession.query(
+        User.username, User.forum_username,
+        Association.child_document_id, Association.child_document_type). \
+        filter(User.id == user_id,
+               Xreport.document_id == Association.child_document_id,
+               Association.parent_document_id == user_id,
+               Association.child_document_type == XREPORT_TYPE). \
+        all()
+
+    if not user_exists:
+        request.errors.add(
+            'body', 'user_id', 'user {0} does not exist'.format(user_id))
+
+    # [
+    #   {
+    #     "child_document_id": 700155,
+    #     "child_document_type": "x",
+    #     "forum_username": "anakreenskyrider",
+    #     "username": "anakreenskyrider"
+    #   },
+    #   ...
+    # ]
+
+    return xreports_for_user_info
+
+    # cant return full documents
+    # xreport_ids_list = []
+    # # print(xreport_ids_list)  # [700155, 700164, 752525, 770436]
+    #
+    # for doc in xreports_for_user_info:
+    #     # print(doc['child_document_id'])
+    #     id = doc['child_document_id']
+    #     xreport_ids_list.append(id)
+    #
+    # xreports_for_user = DBSession.query(Xreport). \
+    #     filter(Xreport.document_id.in_(xreport_ids_list)). \
+    #     all()
+    #
+    #
+    # # return xreports_for_user
+    #
+    # # a = get_documents_for_ids(xreport_ids_list)
+
+
+@resource(path='/xreports-filter', cors_policy=cors_policy)
+class XreportFilterRest(DocumentRest):
+
+    # def __init__(self, request):
+    #     self.request = request
+
+    @view(validators=[validate_user_id])
+    def get(self):
+        if self.request.validated['u']:
+            return get_xreports_for_user(self.request)
 
 
 @resource(collection_path='/xreports', path='/xreports/{id}',
