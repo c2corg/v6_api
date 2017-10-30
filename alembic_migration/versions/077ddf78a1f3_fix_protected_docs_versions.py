62fb38d0860b5feeee39764b6c66f5ceed39b984
Alexandre Saunier <alexandre.saunier@camptocamp.com>
2017-10-30 18:02:26 +0100

    Fix versions of protected/unprotected documents

---
+++ alembic_migration/versions/077ddf78a1f3_fix_protected_docs_versions.py
@@ -0,0 +1,33 @@
+"""Fix protected docs versions
+
+Revision ID: 077ddf78a1f3
+Revises: 9739938498a8
+Create Date: 2017-10-30 12:05:51.679435
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '077ddf78a1f3'
+down_revision = '9739938498a8'
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    op.execute("""
+        with versions_from_archives as (
+          select document_id, max(version) as version
+          from guidebook.documents_archives
+          group by document_id
+        )
+        update guidebook.documents as d
+        set version = va.version
+        from versions_from_archives va
+        where d.document_id = va.document_id""")
+
+
+def downgrade():
+    # Not reversible
+    pass
