77bbd9b0111fc17e87837ba4ebfa17abdec7ffbe
arnaud.morvan@camptocamp.com <arnaud.morvan@camptocamp.com>
2017-11-02 09:30:47 +0100

    Merge alembic heads

---
+++ alembic_migration/versions/6a48dc6c8065_merge_24f8da659c78_and_077ddf78a1f3.py
@@ -0,0 +1,23 @@
+"""Merge 24f8da659c78 and 077ddf78a1f3
+
+Revision ID: 6a48dc6c8065
+Revises: 24f8da659c78, 077ddf78a1f3
+Create Date: 2017-11-02 09:30:09.693455
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6a48dc6c8065'
+down_revision = ('24f8da659c78', '077ddf78a1f3')
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
