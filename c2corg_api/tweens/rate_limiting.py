8598780f489e193c344dd9315ca8d8bc5760aae4
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2019-01-04 21:20:28 +0100

    Add logs to rate limit + exception handling

--- c2corg_api/tweens/rate_limiting.py
+++ c2corg_api/tweens/rate_limiting.py
@@ -7,6 +7,7 @@ from c2corg_api.emails.email_service import get_email_service
 from c2corg_api.views import http_error_handler
 from c2corg_api.models import DBSession
 from c2corg_api.models.user import User
+from smtplib import SMTPAuthenticationError
 
 log = logging.getLogger(__name__)
 
@@ -42,10 +43,19 @@ def rate_limiting_tween_factory(handler, registry):
                 'rate_limiting.limit'))
             user.ratelimit_reset = now + datetime.timedelta(seconds=span)
             user.ratelimit_remaining = limit - 1
+            log.debug('RATE LIMITING, CREATE WINDOW SPAN : {}'.format(
+                user.ratelimit_reset
+            ))
+
         elif user.ratelimit_remaining:
             user.ratelimit_remaining -= 1
+            log.info('RATE LIMITING, REQUESTS REMAINING FOR {} : {}'.format(
+                user.id, user.ratelimit_remaining
+            ))
+
         else:
             # User is rate limited
+            log.info('RATE LIMIT REACHED FOR USER {}'.format(user.id))
 
             # Count how many windows the user has been rate limited
             # and block them is too many.
@@ -57,11 +67,15 @@ def rate_limiting_tween_factory(handler, registry):
                 max_times = int(
                     registry.settings.get('rate_limiting.max_times'))
                 if user.ratelimit_times > max_times:
+                    log.warning('RATE LIMIT BLOCK USER {}'.format(user.id))
                     user.blocked = True
 
                 # An alert message is sent to the moderators
                 email_service = get_email_service(request)
-                email_service.send_rate_limiting_alert(user)
+                try:
+                    email_service.send_rate_limiting_alert(user)
+                except SMTPAuthenticationError:
+                    log.error('RATE LIMIT ALERT MAIL : AUTHENTICATION ERROR')
 
             return http_error_handler(
                 HTTPTooManyRequests('Rate limit reached'), request)
