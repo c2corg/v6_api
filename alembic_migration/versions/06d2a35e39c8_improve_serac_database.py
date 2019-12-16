324473ec395179fbb41f5bb00a439e6ed61ecb07
momomaniac <57792650+momomaniac@users.noreply.github.com>
2019-12-16 22:55:38 +0100

    add missing alembic migration file

---
+++ alembic_migration/versions/06d2a35e39c8_improve_serac_database.py
@@ -0,0 +1,182 @@
+"""Improve SERAC database
+
+Revision ID: 06d2a35e39c8
+Revises: 85a5ed3c76a8
+Create Date: 2019-12-09 13:24:15.936753
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.sql.schema import Table, MetaData
+from alembic_migration.extensions import drop_enum
+
+
+# revision identifiers, used by Alembic.
+revision = '06d2a35e39c8'
+down_revision = '85a5ed3c76a8'
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    # !!!! do not forget xreports_archives
+
+    # convert autonomy enum
+    autonomy_conversions = [
+        ('non_autonomous', 'non_autonomous'),
+        ('autonomous', 'autonomous'),
+        ('initiator', 'autonomous'),
+        ('expert', 'expert')
+    ]
+    old_autonomy_type = sa.Enum('non_autonomous', 'autonomous', 'initiator', 'expert', name='autonomy', schema='guidebook')
+    new_autonomy_type = sa.Enum('non_autonomous', 'autonomous', 'expert', name='autonomy_', schema='guidebook')
+    new_autonomy_type.create(op.get_bind())
+    # op.alter_column('xreports', 'autonomy',
+    #                 type_=new_autonomy_type,
+    #                 existing_type=old_autonomy_type,
+    #                 schema='guidebook')
+    # does not allow automatic casting if table not empty
+
+    op.add_column('xreports',
+                  sa.Column('autonomy_', new_autonomy_type, nullable=True),
+                  schema='guidebook')
+    op.add_column('xreports_archives',
+                  sa.Column('autonomy_', new_autonomy_type, nullable=True),
+                  schema='guidebook')
+
+    xr = Table('xreports', MetaData(),
+               sa.Column('autonomy', old_autonomy_type),
+               sa.Column('autonomy_', new_autonomy_type, nullable=True),
+               schema='guidebook')
+    xra = Table('xreports_archives', MetaData(),
+               sa.Column('autonomy', old_autonomy_type),
+               sa.Column('autonomy_', new_autonomy_type, nullable=True),
+               schema='guidebook')
+    for (old_value, new_value) in autonomy_conversions:
+        op.execute(xr.update()
+                   .where(xr.c.autonomy == op.inline_literal(old_value))
+                   .values(autonomy_=op.inline_literal(new_value)))
+        op.execute(xra.update()
+                   .where(xra.c.autonomy == op.inline_literal(old_value))
+                   .values(autonomy_=op.inline_literal(new_value)))
+
+    op.drop_column('xreports', 'autonomy', schema='guidebook')
+    op.drop_column('xreports_archives', 'autonomy', schema='guidebook')
+    # op.drop_column('xreports_archives', 'avalanche_slope', schema='guidebook')
+    drop_enum('autonomy', schema='guidebook')
+
+    # Rename enum
+    op.execute('ALTER TYPE guidebook.autonomy_ RENAME TO autonomy')
+
+    # Rename column
+    op.alter_column(
+        'xreports',
+        'autonomy_',
+        new_column_name='autonomy',
+        schema='guidebook')
+    op.alter_column(
+        'xreports_archives',
+        'autonomy_',
+        new_column_name='autonomy',
+        schema='guidebook')
+
+    # end of autonomy conversion
+    
+    # convert activity enum
+    activity_conversions = [
+        ('activity_rate_150', 'activity_rate_w1'),
+        ('activity_rate_50', 'activity_rate_w1'),
+        ('activity_rate_30', 'activity_rate_m2'),
+        ('activity_rate_20', 'activity_rate_m2'),
+        ('activity_rate_10', 'activity_rate_y5'),
+        ('activity_rate_5', 'activity_rate_y5'),
+        ('activity_rate_1', 'activity_rate_y5')
+    ]
+    old_activity_type = sa.Enum('activity_rate_150', 'activity_rate_50', 'activity_rate_30', 'activity_rate_20', 'activity_rate_10', 'activity_rate_5', 'activity_rate_1', name='activity_rate', schema='guidebook')
+    new_activity_type = sa.Enum('activity_rate_y5', 'activity_rate_m2', 'activity_rate_w1', name='activity_rate_', schema='guidebook')
+    new_activity_type.create(op.get_bind())
+
+    op.add_column('xreports',
+                  sa.Column('activity_rate_', new_activity_type, nullable=True),
+                  schema='guidebook')
+    op.add_column('xreports_archives',
+                  sa.Column('activity_rate_', new_activity_type, nullable=True),
+                  schema='guidebook')
+
+    xr = Table('xreports', MetaData(),
+               sa.Column('activity_rate', old_activity_type),
+               sa.Column('activity_rate_', new_activity_type, nullable=True),
+               schema='guidebook')
+    xra = Table('xreports_archives', MetaData(),
+               sa.Column('activity_rate', old_activity_type),
+               sa.Column('activity_rate_', new_activity_type, nullable=True),
+               schema='guidebook')
+    for (old_value, new_value) in activity_conversions:
+        op.execute(xr.update()
+                   .where(xr.c.activity_rate == op.inline_literal(old_value))
+                   .values(activity_rate_=op.inline_literal(new_value)))
+        op.execute(xra.update()
+                   .where(xra.c.activity_rate == op.inline_literal(old_value))
+                   .values(activity_rate_=op.inline_literal(new_value)))
+
+    op.drop_column('xreports', 'activity_rate', schema='guidebook')
+    op.drop_column('xreports_archives', 'activity_rate', schema='guidebook')
+    # op.drop_column('xreports_archives', 'avalanche_slope', schema='guidebook')
+    drop_enum('activity_rate', schema='guidebook')
+
+    # Rename enum
+    op.execute('ALTER TYPE guidebook.activity_rate_ RENAME TO activity_rate')
+
+    # Rename column
+    op.alter_column(
+        'xreports',
+        'activity_rate_',
+        new_column_name='activity_rate',
+        schema='guidebook')
+    op.alter_column(
+        'xreports_archives',
+        'activity_rate_',
+        new_column_name='activity_rate',
+        schema='guidebook')
+
+    # end of activity conversion
+
+    op.drop_column('xreports', 'nb_outings', schema='guidebook')
+    op.drop_column('xreports_archives', 'nb_outings', schema='guidebook')
+    # op.drop_column('xreports_archives', 'avalanche_slope', schema='guidebook')
+    drop_enum('nb_outings', schema='guidebook')
+
+    supervision_type = sa.Enum('no_supervision', 'federal_supervision',
+                               'professional_supervision',
+                               name='supervision_type', schema='guidebook')
+    supervision_type.create(op.get_bind())
+    op.add_column('xreports', sa.Column('supervision', supervision_type,
+                                        nullable=True), schema='guidebook')
+    op.add_column('xreports_archives', sa.Column('supervision', supervision_type,
+                                        nullable=True), schema='guidebook')
+    
+    qualification_type = sa.Enum('federal_supervisor', 'federal_trainer',
+                               'professional_diploma',
+                               name='qualification_type', schema='guidebook')
+    qualification_type.create(op.get_bind())
+    op.add_column('xreports', sa.Column('qualification', qualification_type,
+                                        nullable=True), schema='guidebook')
+    op.add_column('xreports_archives', sa.Column('qualification', qualification_type,
+                                        nullable=True), schema='guidebook')
+    # import pdb;pdb.set_trace()
+    pass
+
+
+def downgrade():
+    op.drop_column('xreports', 'supervision', schema='guidebook')
+    op.drop_column('xreports_archives', 'supervision', schema='guidebook')
+    drop_enum('supervision_type', schema='guidebook')
+    op.drop_column('xreports', 'qualification', schema='guidebook')
+    op.drop_column('xreports_archives', 'qualification', schema='guidebook')
+    drop_enum('qualification_type', schema='guidebook')
+
+    nb_outing_type = sa.Enum('nb_outings_4', 'nb_outings_9', 'nb_outings_14', 'nb_outings_15', name='nb_outings', schema='guidebook')
+    nb_outing_type.create(op.get_bind())
+    op.add_column('xreports',
+                  sa.Column('nb_outings', nb_outing_type, nullable=True),
+                  schema='guidebook')
+    pass
