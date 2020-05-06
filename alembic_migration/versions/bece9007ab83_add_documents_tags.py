db7f10a85c480defaf87e1ae1c9e5304a9b06cdc
Alexandre Saunier <alexandre.saunier@camptocamp.com>
2020-05-06 22:34:44 +0200

    Merge alembic migrations for documents tags

--- alembic_migration/versions/bece9007ab83_add_documents_tags.py
+++ alembic_migration/versions/bece9007ab83_add_documents_tags.py
@@ -1,4 +1,4 @@
-"""Add documents_tags table
+"""Add documents tags
 
 Revision ID: bece9007ab83
 Revises: 06d2a35e39c8
@@ -6,6 +6,7 @@ Create Date: 2019-11-10 16:46:13.660416
 
 """
 from alembic import op
+from alembic_migration import extensions
 import sqlalchemy as sa
 
 
@@ -14,6 +15,14 @@ revision = 'bece9007ab83'
 down_revision = '06d2a35e39c8'
 branch_labels = None
 depends_on = None
+
+view_users_for_routes = extensions.ReplaceableObject(
+      'guidebook.users_for_routes',
+      """
+SELECT guidebook.documents_tags.document_id AS route_id, array_agg(guidebook.documents_tags.user_id) AS user_ids
+FROM guidebook.documents_tags
+GROUP BY guidebook.documents_tags.document_id;
+""")
 
 def upgrade():
     op.create_table(
@@ -52,8 +61,12 @@ def upgrade():
         'documents_tags_log', ['written_at'], unique=False, schema='guidebook')
     # TODO add index on user_id/document_id?
 
+    op.create_view(view_users_for_routes)
+
 
 def downgrade():
+    op.drop_view(view_users_for_routes)
+
     op.drop_index(
         op.f('ix_guidebook_documents_tags_user_id'),
         table_name='documents_tags', schema='guidebook')
