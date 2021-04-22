9ef541a2ccb46d65c22c581b670fc9ae6b742ca7
Lionel Besson <lionel.besson@gmail.com>
2021-04-22 10:49:24 +0200

    codacy coverage report

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker-compose exec -u postgres -T postgresql /v6_api/scripts/create_user_db_test.sh
-docker-compose exec api .build/venv/bin/pytest ${@:-"--cov=c2corg_api"}
+docker-compose exec api .build/venv/bin/pytest --cov-report term --cov-report xml ${@:-"--cov=c2corg_api"}
