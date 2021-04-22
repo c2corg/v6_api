4f517e2951c1764f1a9369379049a0fb54a6ab22
Lionel Besson <lionel.besson@gmail.com>
2021-04-22 11:18:08 +0200

    Coverage

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,3 +2,7 @@
 
 docker-compose exec -u postgres -T postgresql /v6_api/scripts/create_user_db_test.sh
 docker-compose exec api .build/venv/bin/pytest --cov-report term --cov-report xml ${@:-"--cov=c2corg_api"}
+
+# Copy coverage output to local
+API_CONTAINER_ID=$(docker-compose  ps -q api)
+docker cp $API_CONTAINER_ID:/var/www/coverage.xml .
