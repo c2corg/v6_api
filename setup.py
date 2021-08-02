7a6cf28ac42ceb30eb25b86197e318dcb66f9626
Lionel Besson <lionel.besson@gmail.com>
2021-08-02 17:49:59 +0200

    CHANGES.txt was removed

--- setup.py
+++ setup.py
@@ -5,13 +5,11 @@ from setuptools import setup, find_packages
 here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'README.md')) as f:
     README = f.read()
-with open(os.path.join(here, 'CHANGES.txt')) as f:
-    CHANGES = f.read()
 
 setup(name='c2corg_api',
       version='0.0',
       description='c2corg_api',
-      long_description=README + '\n\n' + CHANGES,
+      long_description=README,
       classifiers=[
         "Programming Language :: Python",
         "Framework :: Pyramid",
