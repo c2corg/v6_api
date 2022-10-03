0e41aa546636fc2829c4aee1feac4f5b7355e393
JP <jphilippe.ds@gmail.com>
2022-10-04 00:21:40 +0200

    Update filter

--- c2corg_api/search/search_filters.py
+++ c2corg_api/search/search_filters.py
@@ -1,16 +1,16 @@
 import logging
 import math
-
-from pyproj import Transformer
 import re
-from c2corg_api.models.outing import OUTING_TYPE
-from c2corg_api.search.mapping_types import reserved_query_fields
+from datetime import datetime
 from functools import partial
 
-from c2corg_api.search import create_search, search_documents, \
-    get_text_query_on_title
-from elasticsearch_dsl.query import Range, Term, Terms, Bool, GeoBoundingBox, \
-    Missing, Script
+from c2corg_api.models.outing import OUTING_TYPE
+from c2corg_api.search import (create_search, get_text_query_on_title,
+                               search_documents)
+from c2corg_api.search.mapping_types import reserved_query_fields
+from elasticsearch_dsl.query import (Bool, GeoBoundingBox, Missing, Range,
+                                     Script, Term, Terms)
+from pyproj import Transformer
 
 log = logging.getLogger(__name__)
 
@@ -277,27 +277,32 @@ def create_period_filter(field, query_term):
         01-01,01-03
 
     """
+    milliseconds_in_one_year = int(365.2425 * 24 * 3600 * 1000)
     query_terms = query_term.split(',')
     range_values = list(map(parse_date_without_year, query_terms))
     range_values = [t for t in range_values if t is not None]
 
-    log.debug(query_terms)
-    log.debug(range_values)
+    def milliseconds_since_first_day_of_year(month_and_day):
+        seconds_since_epoch = datetime.strptime('2000-{}'.format(month_and_day),'%Y-%m-%d').timestamp()
+
+        return int(1000 * seconds_since_epoch) % milliseconds_in_one_year
 
     n = len(range_values)
 
-    log.debug(n)
-
-    if n == 0 or n==1:
+    if n != 2:
         return None
-    else:
-        return Script(
-            script="doc['date_end'].value%31556952000 >= min && doc['date_start'].value%31556952000 <= max",
-            params= {
-						"min": 23620896000,
-						"max": 24854040000
-					}
-        )
+
+    return Script(
+        script="doc['{0}'].value%{2} >= min && doc['{1}'].value%{2} <= max".format(
+            field.field_date_end,
+            field.field_date_start,
+            milliseconds_in_one_year
+        ),
+        params= {
+            "min": milliseconds_since_first_day_of_year(range_values[0]),
+            "max": milliseconds_since_first_day_of_year(range_values[1])
+        }
+    )
 
 
 def create_date_filter(field, query_term):
