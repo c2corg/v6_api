1e6171edee935bd3cd803994745ea36b93c1597d
Kévin Liagre <k.liagre@yogosha.com>
2024-03-20 13:45:44 +0100

    Update docker-compose services

--- scripts/create_user_db_test.sh
+++ scripts/create_user_db_test.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-DBNAME="c2corg_${USER}_tests"
-[ -z "$USER" ] && DBNAME="c2corg_tests"
+DBNAME="c2corg_tests"
 
 if [ "$( psql -tAc "SELECT 1 FROM pg_database WHERE datname='${DBNAME}'" )" = '1' ]
 then
@@ -10,13 +9,13 @@ else
     echo "Create test database"
 
     psql <<EOF
-create database ${DBNAME} owner "www-data";
+create database ${DBNAME} owner "postgres";
 \c ${DBNAME}
 create extension postgis;
-create schema guidebook authorization "www-data";
-create schema users authorization "www-data";
-create schema sympa authorization "www-data";
-create schema alembic authorization "www-data";
+create schema guidebook authorization "postgres";
+create schema users authorization "postgres";
+create schema sympa authorization "postgres";
+create schema alembic authorization "postgres";
 \q
 EOF
 fi
