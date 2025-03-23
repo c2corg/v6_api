9dd938b3a5ab83b35833b06d229e7efdf74f4473
Romain Dartigues <romain.dartigues@gmail.com>
2025-03-23 10:48:37 +0100

    fix: Alembic tests workaround
    > alembic.util.exc.CommandError: Multiple head revisions are present for given argument 'head'
    
    Ref: 6f0b867

---
+++ alembic_migration/versions/889705aa08df_.py
@@ -0,0 +1,23 @@
+"""empty message
+
+Revision ID: 889705aa08df
+Revises: 1d851410e3af, b59d3efaf2a1
+Create Date: 2025-03-23 10:46:16.106652
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '889705aa08df'
+down_revision = ('1d851410e3af', 'b59d3efaf2a1')
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
