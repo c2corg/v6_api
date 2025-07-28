46486010fb2be9179b885bd94190f965e36064ed
Leo Gourdin <leo.gourdin@smart-origin.com>
2025-07-28 15:40:56 +0200

    [rebase] fix minor conflict

--- c2corg_api/__init__.py
+++ c2corg_api/__init__.py
@@ -303,7 +303,6 @@ def process_new_waypoint(mapper, connection, geometry):
 @event.listens_for(Route, "after_insert")
 @event.listens_for(Route, "after_update")
 def calculate_route_duration(mapper, connection, route):
-    # pylint: disable=too-complex,too-many-branches,too-many-statements
     """
     Calcule la durée estimée d'un itinéraire
     après son insertion ou sa mise à jour,
