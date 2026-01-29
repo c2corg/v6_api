9523ffedf6666d4a06ec5ef02e61ff82107444bb
Sébastien Favre <sebastien.favre@meritis.fr>
2026-01-29 17:26:24 +0100

    Change path of default env files in entrypoint.sh

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-./scripts/env_replace env.default .env < production.ini.in > production.ini
+set -e
+
+./scripts/env_replace config/env.default .env < production.ini.in > production.ini
 
 gunicorn --paste production.ini -u www-data -g www-data -b:8080
