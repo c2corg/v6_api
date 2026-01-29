cc9acc49d7a87c2dc52eac38ecce6e858be5cbd1
Sébastien Favre <sebastien.favre@meritis.fr>
2026-01-29 17:30:41 +0100

    Add common.ini generation to entrypoint.sh

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -3,5 +3,6 @@
 set -e
 
 ./scripts/env_replace config/env.default .env < production.ini.in > production.ini
+./scripts/env_replace config/env.default .env < common.ini.in > common.ini
 
 gunicorn --paste production.ini -u www-data -g www-data -b:8080
