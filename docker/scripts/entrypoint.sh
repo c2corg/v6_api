215905eac3468821adfafb2c5709c654e6e83801
Sébastien Favre <sebastien.favre@meritis.fr>
2026-01-29 17:38:28 +0100

    Remove main command from entrypoint.sh and use Dockerfile CMD

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -5,4 +5,4 @@ set -e
 ./scripts/env_replace config/env.default .env < production.ini.in > production.ini
 ./scripts/env_replace config/env.default .env < common.ini.in > common.ini
 
-gunicorn --paste production.ini -u www-data -g www-data -b:8080
+exec "$@"
